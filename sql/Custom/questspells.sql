4627f9a3250a0ef00923d8095ddf8f7021759e42
LilleCarl <lillecarl@gmail.com>
2013-10-28 22:12:11 +0100

    Quest spells added, will need feedback in the future!

---
+++ sql/Custom/questspells.sql
@@ -0,0 +1,73 @@
+/* Warlock: */
+INSERT INTO `npc_trainer` VALUES ('26331', '688', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26331', '691', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26331', '697', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26331', '712', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26331', '1122', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26331', '1373', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26331', '1413', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26331', '5784', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26331', '5785', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26331', '7763', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26331', '11519', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26331', '11520', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26331', '18540', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26331', '20700', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26331', '23160', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26331', '23161', '0', '0', '0', '0');
+/* Druid: */
+INSERT INTO `npc_trainer` VALUES ('26324', '1126', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26324', '1446', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26324', '8947', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26324', '9884', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26324', '19179', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26324', '40120', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26324', '40123', '0', '0', '0', '0');
+/* Mage: */
+INSERT INTO `npc_trainer` VALUES ('26326', '1460', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26326', '10140', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26326', '10143', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26326', '28272', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26326', '28285', '0', '0', '0', '0');
+/* Hunter:*/
+INSERT INTO `npc_trainer` VALUES ('26325', '1579', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26325', '5300', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26325', '23356', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26325', '23357', '0', '0', '0', '0');
+/* Shaman: */
+INSERT INTO `npc_trainer` VALUES ('26330', '2075', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26330', '3599', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26330', '5394', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26330', '5396', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26330', '8071', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26330', '8073', '0', '0', '0', '0');
+/* Priest: */
+INSERT INTO `npc_trainer` VALUES ('26328', '2651', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26328', '2652', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26328', '2944', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26328', '6346', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26328', '9035', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26328', '10797', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26328', '10937', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26328', '10938', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26328', '13896', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26328', '13908', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26328', '18137', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26328', '32548', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26328', '32676', '0', '0', '0', '0');
+/* Rogue: */
+INSERT INTO `npc_trainer` VALUES ('26329', '2995', '0', '0', '0', '0');
+/* Paladin: */
+INSERT INTO `npc_trainer` VALUES ('26327', '5502', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26327', '5503', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26327', '7328', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26327', '7329', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26327', '13819', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26327', '13820', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26327', '23215', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26327', '34766', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26327', '34767', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26327', '34768', '0', '0', '0', '0');
+/* Warrior: */
+INSERT INTO `npc_trainer` VALUES ('26332', '8121', '0', '0', '0', '0');
+INSERT INTO `npc_trainer` VALUES ('26332', '8616', '0', '0', '0', '0');
