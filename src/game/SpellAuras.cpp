92d7e3706ac422fa68fc691a8ed20f71a5a917bb
Salja <salja8840@gmail.com>
2014-01-20 14:22:49 +0100

    [DEV] Fix Stuns in Flight (Workaround)
    Signed-off-by: Salja <salja8840@gmail.com>
    (cherry picked from commit 7ed37007e5fe287636a410903c9530e7b4d4476c)

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -3636,6 +3636,9 @@ void Aura::HandleAuraModDisarm(bool apply, bool Real)
 
 void Aura::HandleAuraModStun(bool apply, bool Real)
 {
+    if (GetTarget()->IsTaxiFlying())
+        return;
+
     if (!Real)
         return;
 
