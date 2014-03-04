064b33a27ad0300b4d4f18fdef5e0b11f278aae1
LilleCarl <lillecarl@gmail.com>
2014-03-04 19:28:51 +0100

    Now all files have proper headers.

--- src/scripts/npc_teleporter.cpp
+++ src/scripts/npc_teleporter.cpp
@@ -1,3 +1,21 @@
+/*
+* See AUTHORS file for Copyright information
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 #include "Precompiled.h"
 #include "ArenaTeam.h"
 #include "Settings.h"
@@ -360,7 +378,7 @@ public:
             pCPlayer->AddGossipMenuItem(Icon::SWORDS,   "Icon::SWORDS   ", GOSSIP_SENDER_MAIN, 0);
         }
 
-        if (pPlayer->PlayerTalkClass->GetGossipMenu().MenuItemCount() > 0)
+        if (!pPlayer->PlayerTalkClass->GetGossipMenu().Empty())
             pPlayer->PlayerTalkClass->SendGossipMenu("Your wish, my law!", pCreature->GetObjectGuid());
 
         return true;
